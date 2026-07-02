88e59e907e92d7d16688db0f9efc348744a317ad
Atul <upadhyayatul635@gmail.com>
2026-07-02 11:26:58 +0000

    Bump version to 1.0.1

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
     project: 'EKS ArgoCD Capstone',
     author: 'Atul Upadhyay',
     github: 'github.com/atulupadhyay2004',
-    version: process.env.APP_VERSION || '1.0.0',
+    version: process.env.APP_VERSION || '2.0.0',
     build: process.env.BUILD_NUMBER || 'local',
     pod: process.env.HOSTNAME || 'unknown',
     environment: process.env.NODE_ENV || 'development',
